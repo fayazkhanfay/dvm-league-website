a59bfc324d70464ee20ce5712b5e64e24bf77f19
Fayaz <fayazkhanfay@gmail.com>
2025-10-06 11:46:51 -0400

    feat: integrate Sentry configuration into Next.js for enhanced error tracking and monitoring
    Updated the Next.js configuration to include Sentry integration, allowing for improved error tracking and monitoring capabilities. Configured Sentry with specific project settings to optimize performance and error handling.

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from "@sentry/nextjs"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -11,4 +13,12 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  org: "dvm-league",
+  project: "javascript-nextjs",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+  tunnelRoute: "/monitoring",
+})
